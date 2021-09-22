b1c3df9d1adbe25b10367af6f3b7ee43f0fb7682
vijay0764 <90964627+vijay0764@users.noreply.github.com>
2021-09-22 16:57:56 +0530

    Add files via upload

---
+++ even_odd_coversion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << " Enter a number   ";
+    cin >> n;
+    cout << "\n";
+    if(n%2==0 || n==0)
+        {
+        cout <<"It's Even number \n";
+
+        }
+    else {
+        cout <<"It's Odd number \n" ;
+        }
+    return 0;
+}
